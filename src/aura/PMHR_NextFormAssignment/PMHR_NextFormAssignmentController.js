1aabab5364f51443954bd18eba827e87fde32f9b
Michaard <graniczkamichal@gmail.com>
2020-01-14 12:06:37 +0100

    + Sword and Shield support update + Minor code flexibility fixes + New tests and coverage

--- src/aura/PMHR_NextFormAssignment/PMHR_NextFormAssignmentController.js
+++ src/aura/PMHR_NextFormAssignment/PMHR_NextFormAssignmentController.js
@@ -5,7 +5,9 @@
 
     levelUp : function(component, event, helper) {
         let index = event.getSource().get("v.name");
-        let nextFormId = component.get("v.nextAvailableForms")[index].Id;
-        helper.setPokemonForm(component, nextFormId);
+        let nextForm = component.get("v.nextAvailableForms")[index];
+        let nextFormId = nextForm.id;
+        let nextFormVariant = nextForm.variant;
+        helper.setPokemonForm(component, nextFormId, nextFormVariant);
     }
 })
